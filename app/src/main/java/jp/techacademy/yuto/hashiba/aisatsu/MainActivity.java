43b6c297b94b0f64351c2745176c712eeffb6953
YuHashi <lacrosse-otafuku@hotmail.co.jp>
2018-09-16 22:11:35 +0900

    02

--- app/src/main/java/jp/techacademy/yuto/hashiba/aisatsu/MainActivity.java
+++ app/src/main/java/jp/techacademy/yuto/hashiba/aisatsu/MainActivity.java
@@ -1,15 +1,52 @@
 package jp.techacademy.yuto.hashiba.aisatsu;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //hoge
+        Button button01 = (Button) findViewById(R.id.button01);
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("UI_PARTS", "ボタンをタップしました");
+                showTimePickerDialog();
+            }
+        });
+
     }
+
+    public void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Log.d("UI-PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                        TextView mTextView;
+                        mTextView = (TextView) findViewById(R.id.TextView01);
+                        mTextView.setText(String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                    }
+                },
+                13, // 初期値（時間）
+                0,  // 初期値（分）
+                true);
+        timePickerDialog.show();
+
+
+
+    }
+
+
 }
